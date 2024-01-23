9e33baf20d518190a90bfb9435bc0d6da6ea1a30
cristianvernal <cr.vernal@duocuc.cl>
2024-01-23 17:47:59 -0300

    Conceptos basicos

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 console.log("Hola Mundo")
+
+// tipos de datos en JS
+//String: "entre comillas solo texto"
+//boolean: true o false
+//Null: nulo.(vacia)
+//Number: 123546
+// undefined. variable no definida
+// Object: objeto. agrupan datos.
+
+// variables
+// var, let o const
+
+// var miPrimeraVariable = 'lala'
+
+let miPrimeraVariable = 'Mi primera variable!'
+console.log(miPrimeraVariable);
+
+//Mutabilidad
+miPrimeraVariable = 'Esto a cambiado' //cambiando el valor de la variable
+console.log(miPrimeraVariable);
+
+let miBoolean = true;
+let miBoolean2 = false;
+
+let miNumero = 0;
+let miNumero2 = 12;
+let miNumero3 = -323;
+
+console.log(miNumero, miNumero2, miNumero3, miBoolean, miBoolean2);
+
+let undefined
+console.log(undefined)
+
+let nulo = null
+console.log(nulo);
+
+//objetos: agrupacion de datos, hacen sentido entre si. Los objetos no tienen variables, tienen propiedades.
+
+//objeto vacio
+const miPrimerObjeto = {}
+
+//objeto
+const miObjeto = {
+    unNumero: 12,
+    unString: 'esta cadena de caracteres',
+    unaCondicion: true
+}
+
+console.log(miObjeto); //si quiero llamar una sola propiedad se utiliza el punto
+
+//Arreglos.
+
+const arrVacio = []
+const arr = [1,2,'hola mundo', miObjeto]
+
+
+arrVacio.push(5) //agregar elemento
+arrVacio.push(4) //agregar elemento
+arrVacio.push('hola') //agregar elemento
+arrVacio.push(23) //agregar elemento
+arrVacio.push(miPrimeraVariable)
+console.log(arrVacio);
